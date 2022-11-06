19a8d5edecb1d5964cd853270cfa15902491b84c
BOT-OF <92382214+BOT-OF@users.noreply.github.com>
2022-11-06 06:24:49 -0800

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,66 @@
+require('dotenv').config();
+
+import Fastify from 'fastify';
+import FastifyCors from '@fastify/cors';
+
+import books from './routes/books';
+import anime from './routes/anime';
+import manga from './routes/manga';
+import comics from './routes/comics';
+import lightnovels from './routes/light-novels';
+import movies from './routes/movies';
+import meta from './routes/meta';
+
+import RapidCloud from './utils/rapid-cloud';
+import BilibiliUtilis from './utils/bilibili';
+import CrunchyrollManager from './utils/crunchyroll-token';
+
+(async () => {
+  const PORT = Number(process.env.PORT);
+  if (process.env.ACCESS_TOKEN !== undefined)
+    (
+      global as typeof globalThis & {
+        CrunchyrollToken: string;
+      }
+    ).CrunchyrollToken = (await CrunchyrollManager.create()).token!;
+
+  const fastify = Fastify({
+    logger: true,
+  });
+  await fastify.register(FastifyCors, {
+    origin: '*',
+    methods: 'GET',
+  });
+
+  await fastify.register(books, { prefix: '/books' });
+  await fastify.register(anime, { prefix: '/anime' });
+  await fastify.register(manga, { prefix: '/manga' });
+  //await fastify.register(comics, { prefix: '/comics' });
+  await fastify.register(lightnovels, { prefix: '/light-novels' });
+  await fastify.register(movies, { prefix: '/movies' });
+  await fastify.register(meta, { prefix: '/meta' });
+
+  //await fastify.register(new RapidCloud().returnSID, { prefix: '/utils' });
+  await fastify.register(new BilibiliUtilis('en_US').returnDASH, { prefix: '/utils' });
+  await fastify.register(new BilibiliUtilis('en_US').returnVTT, { prefix: '/utils' });
+
+  try {
+    fastify.get('/', (_, rp) => {
+      rp.status(200).send('ANIMETV7 API WORKING 🐉');
+    });
+    fastify.get('*', (request, reply) => {
+      reply.status(404).send({
+        message: '',
+        error: 'page not found',
+      });
+    });
+
+    fastify.listen({ port: PORT, host: '0.0.0.0' }, (e, address) => {
+      if (e) throw e;
+      console.log(`server listening on ${address}`);
+    });
+  } catch (err: any) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+})();
